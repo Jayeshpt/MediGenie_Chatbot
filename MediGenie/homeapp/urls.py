fa6564c345aac55ed09893461b20dfba6d1bc039
Jayesh PT <jayeshpt2000@gmail.com>
2023-09-02 15:58:14 +0530

    chatbot feature updation

--- MediGenie/homeapp/urls.py
+++ MediGenie/homeapp/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('upload/', views.upload_file, name='upload_file'),
     path('data_table/', views.show_data, name='show_data'),
+    path('chatbot/',views.chatbot,name='chatbot'),
+    path('get_response', views.get_response, name='get_response'),
 
 ]
